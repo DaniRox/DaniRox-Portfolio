e26b2ad8471d0359a0f100b1653d9cf8595a0cfe
DaniRox <daniroxescobar@gmail.com>
2023-06-10 20:33:51 -0300

    agregando estilos inicio

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector(".nav");
 const btnMenu = document.querySelector(".btn-nav");
 
-function displayMenu(){
+function displayMenu() {
     menu.style.display = "flex";
 }
