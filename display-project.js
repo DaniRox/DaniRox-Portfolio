c5252bbdfe1cd675c5e3d7fca1b2a5a204875b39
DaniRox <daniroxescobar@gmail.com>
2023-06-12 23:40:58 -0300

    agregando funciones en gallery

---
+++ display-project.js
@@ -0,0 +1,73 @@
+const infoGallery1 = document.querySelector("#gallery-info1");
+const infoGallery2 = document.querySelector("#gallery-info2");
+const infoGallery3 = document.querySelector("#gallery-info3");
+const infoGallery4 = document.querySelector("#gallery-info4");
+const infoGallery5 = document.querySelector("#gallery-info5");
+const infoGallery6 = document.querySelector("#gallery-info6");
+const infoProject1 = document.querySelector("#project-info1");
+const infoProject2 = document.querySelector("#project-info2");
+const infoProject3 = document.querySelector("#project-info3");
+const infoProject4 = document.querySelector("#project-info4");
+const infoProject5 = document.querySelector("#project-info5");
+const infoProject6 = document.querySelector("#project-info6");
+
+
+function displayInfo1() {
+    if(infoGallery1.style.display === "none" && infoProject1.style.display === "none"){
+        infoGallery1.style.display = "flex";
+        infoProject1.style.display = "flex";
+    }else {
+        infoGallery1.style.display = "none";
+        infoProject1.style.display = "none";
+    }
+}
+
+function displayInfo2() {
+    if(infoGallery2.style.display === "none" && infoProject2.style.display === "none"){
+        infoGallery2.style.display = "flex";
+        infoProject2.style.display = "flex";
+    }else {
+        infoGallery2.style.display = "none";
+        infoProject2.style.display = "none";
+    }
+}
+
+function displayInfo3() {
+    if(infoGallery3.style.display === "none" && infoProject3.style.display === "none"){
+        infoGallery3.style.display = "flex";
+        infoProject3.style.display = "flex";
+    }else {
+        infoGallery3.style.display = "none";
+        infoProject3.style.display = "none";
+    }
+}
+
+function displayInfo4() {
+    if(infoGallery4.style.display === "none" && infoProject4.style.display === "none"){
+        infoGallery4.style.display = "flex";
+        infoProject4.style.display = "flex";
+    }else {
+        infoGallery4.style.display = "none";
+        infoProject4.style.display = "none";
+    }
+}
+
+function displayInfo5() {
+    if(infoGallery5.style.display === "none" && infoProject5.style.display === "none"){
+        infoGallery5.style.display = "flex";
+        infoProject5.style.display = "flex";
+    }else {
+        infoGallery5.style.display = "none";
+        infoProject5.style.display = "none";
+    }
+}
+
+function displayInfo6() {
+    if(infoGallery6.style.display === "none" && infoProject6.style.display === "none"){
+        infoGallery6.style.display = "flex";
+        infoProject6.style.display = "flex";
+    }else {
+        infoGallery6.style.display = "none";
+        infoProject6.style.display = "none";
+    }
+}
