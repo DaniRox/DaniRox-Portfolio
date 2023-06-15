d8e23700dbaa104bf075e4525474cd7231f011d9
DaniRox <daniroxescobar@gmail.com>
2023-06-15 14:29:58 -0300

    agregando estilos

---
+++ displayMenu.js
@@ -0,0 +1,13 @@
+const navMenu = document.querySelector(".nav-menu");
+const btnMenu = document.getElementById("nav-button");
+
+
+btnMenu.addEventListener("click", displayMenu);
+
+function displayMenu() {
+    if(navMenu.style.display === "none"){
+        navMenu.style.display = "flex";
+    }else {
+        navMenu.style.display = "none";
+    }
+}
