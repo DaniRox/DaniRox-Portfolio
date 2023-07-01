27c7ba9d3f2583cc1ed619a6e747b49cc86306e6
DaniRox <daniroxescobar@gmail.com>
2023-07-01 00:04:58 -0300

    agregando estilos

---
+++ msj-enviado.js
@@ -0,0 +1,14 @@
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const mensaje = document.getElementById("mensaje");
+
+const btnEnviar = document.querySelector(".btn-enviar");
+/*
+function msjEnviado() {
+    if(nombre.value === "" && email.value === "" && mensaje.value === ""){
+        console.log("aun debe llenar los campos requeridos");
+    } else {
+        location.href="./html/formulario-enviado.html";
+    }
+    //location.href="./html/formulario-enviado.html";
+}*/
