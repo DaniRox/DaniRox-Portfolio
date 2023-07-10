d93814ac91b7a1bbc2be17899165b584817c2a9d
DaniRox <daniroxescobar@gmail.com>
2023-07-09 21:31:39 -0300

    modificacion en el navbar

--- js/btn-form.js
+++ js/btn-form.js
@@ -5,7 +5,7 @@ const formMensaje = document.getElementById("mensaje");
 const btnForm = document.getElementById("form-btn");
 
 
-btnForm.addEventListener("click", desabilitarBtn())
+btnForm.addEventListener("click", desabilitarBtn)
 
 function desabilitarBtn() {
     if(formNombre.value === "" && formEmail.value === "" && formAsunto.value === "" && formMensaje.value === "") {
