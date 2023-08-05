6cb351ae8a1cfc42a8485dd569e248d579f6ec2a
DaniRox <daniroxescobar@gmail.com>
2023-08-05 15:32:09 -0300

    estilos header y footer

---
+++ js/display-menu.js
@@ -0,0 +1,13 @@
+const menuNav = document.getElementById("menu-nav");
+const btnMenu = document.getElementById("btn-menu-nav");
+
+
+function displayMenu(){
+    if(menuNav.style.display === "flex"){
+        menuNav.style.display = "none";
+    } else {
+        menuNav.style.display = "flex";
+    }
+}
+
+btnMenu.addEventListener("click", displayMenu);
